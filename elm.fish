6ed182cc5f56a1ecc52e7a5f006c711ef0f2f2ee
Jesse Cooke <jesse@relativepath.io>
2019-08-06 16:50:37 -0700

    Fish: add some subcommands

--- elm.fish
+++ elm.fish
@@ -2,3 +2,7 @@ complete -e -c elm
 
 complete -c elm -s h -l help
 complete -c elm -l version
+
+set -l __fish_elm_subcommands repl init reactor make install bump diff publish
+complete -c elm -f -c elm -n '__fish_use_subcommand' -a '$__fish_elm_subcommands'
+complete -c elm -x -c elm -n '__fish_seen_subcommand_from help' -a '$__fish_elm_subcommands'
